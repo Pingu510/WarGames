ada7eb68d3b2514da11ac8fe5f60004852ee39a9
Frida Gidlund <frida.gidlund@yaelev.se>
2017-11-08 20:10:52 +0100

    Layouten

---
+++ WarGames/Countries.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarGames
+{
+    public abstract class Countries
+    {
+        public string CountryName { get; set; }
+        public int CountryEndurance { get; set; }
+
+        public Countries()
+        {
+
+        }
+    }
+}
